158178a3a884dd3b51017dd7a6c8f2aa1c2865c1
Cr1sseb <criss.alvaradoo02@gmail.com>
2025-11-08 03:06:25 -0300

    Primer commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\CategoriaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('almacen/categoria', 'CategoriaController'::class);
